7a50294f80878deff17d187c5eeffb3661bbb80e
Cordyceps <andreproenca13@gmail.com>
2026-09-23 15:53:39 +0100

    feat: clean dead code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-// import Saudacao from './components/Saudacao.jsx'
-// import './App.css'
-// import Contador from './components/Contador.jsx';
-// import Cartao from './components/Cartao.jsx';
 import ListaFilmes from './components/ListaFilmes.jsx'
 
 function App() {
@@ -14,13 +10,6 @@ function App() {
         {nomes.map(n => <li key={n}>{n}</li>)}
       </ul>
       <ListaFilmes/>
-      {/* <Contador />
-      <Contador />
-      <Saudacao nome='André' />
-      <Saudacao nome='Pedro' />
-      <Saudacao nome='Tiago' />
-      <Cartao titulo='Batman' ano='1999' />
-      <Cartao titulo='Avatar' ano='2020' /> */}
     </div>
   )
 }
