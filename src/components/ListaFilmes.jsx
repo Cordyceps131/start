a707ba868b718178462222baee550d27fd9b808b
Cordyceps <andreproenca13@gmail.com>
2026-09-23 15:51:07 +0100

    feat: condicional rendering

--- src/components/ListaFilmes.jsx
+++ src/components/ListaFilmes.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import Cartao from './Cartao.jsx'
 function ListaFilmes() {
+    const [filtro, setFiltro] = useState(false);
     const filmes = [
         { id: 1, titulo: 'Batman', ano: 2005 },
         { id: 2, titulo: 'Matrix', ano: 1999 },
         { id: 3, titulo: 'Avatar', ano: 2009 },
     ];
     return (<>
-        {filmes.map(f => <Cartao key={f.id} titulo={f.titulo} ano={f.ano}/>)}
+        <button onClick={() => setFiltro(!filtro)}>Mostrar só filmes depois de 2000</button>
+        {filmes.filter(f => !filtro || f.ano > 2000).map(f => <Cartao key={f.id} titulo={f.titulo} ano={f.ano} />)}
     </>)
 }
 
